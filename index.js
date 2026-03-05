62fe1e4856bfbb618b1050f5415b8ff135f115e8
Rachayita Bora <rachayitabora2005@gmail.com>
2026-03-05 16:06:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello from Node.js");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World mckv1')
+})
+
+app.listen(4000, ()=>{
+    console.log("Started server");
+}
+)
